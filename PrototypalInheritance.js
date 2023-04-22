b2d73d227fa3ce9ba64ba6734e7c0acce588c1e9
SudamAtCAW <sudam.swain@cawstudios.com>
2023-04-22 06:45:39 +0530

    abc

--- PrototypalInheritance.js
+++ PrototypalInheritance.js
@@ -165,4 +165,89 @@ array.__proto__ //base array, it is pointing to the father
 //arrays and functions are obects in JS. multipliedby5 is a special type of object that is a callable object that instead of having call, apply and bind directly as it's properties
 //uses prototypal inheritance to actually inherit from the base function
 
-//__proto__ points to prototype{...} up the cain,and __proto__ lives inside prototype object, tis
+//__proto__ points to prototype{...} up the cain,and __proto__ lives inside prototype object, 
+//We should never do prototype __proto__=some type of an object because we shuld never use this ,for performance reason in JS
+
+//The safe way to inhertance
+
+let human = {
+    mortal: true
+}
+
+let socrates = Object.create(human) //this is one of the ways for inheritance, we creted using object create a prototype chain up to human
+
+//object human will be equal to socrates
+console.log(socrates)  //{} empty object
+//Add a property in socrates
+socrates.age= 45;
+console.log(socrates) //{age:45}
+consoole.log(socrates.mortal) //true
+
+console.log(human.isPrototypeOf(socrates)) //true
+
+//------>>> Only the functions have the prototype property.  <<<------
+
+//Every function has a prototype property and it references to an object used to attach properties that will be inherited by objects further down
+//The last object in the chain is this built in Object.prototype
+
+//Object is a function as it has the prototype property, 
+Object.prototype //base object, That's the very last object that we can look for properties on before point.
+//cretaed obejcts and arrays even if they are Objects, they don't have the prototype property because they are not a function
+
+'string'.prototype //undefined , when we tried this, underneath the hood, when we try to access a method on the 'string' JS is going to convert it into a string object and look up the prototype chain for these methods
+//that way we can perform all these actions on the string type.
+String.prototype // String object , 
+
+//Everything in JS is an object. and arrays and functions in jS are objects they inherit through the prototype chain from the base object. We can go up the prototype chain looking forproperties
+//on this prototype property. This prototype property also has the __proto__ property inside of it, that links higher up to the next prototype.
+
+//Exercise:
+Array.prototype.map = function()  { // what happens with arrow function here?
+    arr = [];
+    //here this will be referred to whoever called this, whatevere to the left of this
+    for (let i = 0; i < this.length; i++) {
+        //here we are getting each property of the array by it's index number and concatinating that with the map, Because of type coercion, we can concate a string with a number
+      arr.push((this[i]+'🗺'));
+    }
+    //we havee to return the array
+    return arr;
+  }
+  console.log([1,2,3].map())
+  
+  //Date object => to have method .yesterday() which shows you yesterday's day in 'YYYY-MM-DD' format.
+  //So the date object doesn't have lastyear property. By writing this below line we can access the lastyear property for Date object
+  Date.prototype.lastYear = function(){
+    //As we are currently inside Date object this will point out to Date object and use the methods that Date object has,
+    //by substacting one from the current year(getFullYear) we will get last year
+    return this.getFullYear() - 1;
+  }
+  
+  new Date('1900-10-10').lastYear() //1899
+//the below arrow function will through error as now this is lexically scoped. This will refer to the actual function that called us, This is one of the rare cases that we want this to be 
+//determined at call time, when we actually run this function.
+  Date.prototype.lastYear = () =>{
+    console.log(this)
+    return this.getFullYear() -1;
+  }
+
+  //Problem\
+//   How would you be able to create your own .bind() method using call or apply.
+
+// Hint:
+
+Function.prototype.bind = function(){
+}
+
+
+//Solution
+
+//here by doing this below line we are creating our own bind method and by prototype we can access it later
+  Function.prototype.bind = function(whoIsCallingMe){
+    //by assigning the value of this, even if the scope is changed, we will still be able to point out to the required object
+    const self = this;
+    return function(){
+        //self is pointing to the object to which this was pointing
+      return self.apply(whoIsCallingMe, arguments);
+    };
+  }
+  // don't worry if you didn't get this... we will expand on this later.
